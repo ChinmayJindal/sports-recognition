dcdc24bd8953de6b0f9815c97779b5347b178fa2
Chinmay <cs12b1012@iith.ac.in>
2015-11-29 14:08:01 +0530

    sampling factor added to 4

--- Util.py
+++ Util.py
@@ -4,7 +4,7 @@ from commands import getstatusoutput as gso
 # dumps dense trajectory features at the video location
 def dumpDTF(videoPath):
 	outputPath = os.path.splitext(videoPath)[0] + '_features.txt'
-	command = './DenseTrack ' + videoPath + ' > ' + outputPath
+	command = './DenseTrack ' + videoPath + ' -I 4 > ' + outputPath
 	out = gso(command)
 
 	if out[0]!=0:
