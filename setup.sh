33f2cada1277bf8b50e591b27afb7fbd069b7344
Sanyam Kapoor <1sanyamkapoor@gmail.com>
2015-11-29 13:09:10 +0530

    added scikit-learn to setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 sudo add-apt-repository -y ppa:mc3man/trusty-media
 sudo apt-get -y update
-sudo apt-get -y install ffmpeg libavdevice-dev libopencv-dev python-opencv
-
+sudo apt-get -y install ffmpeg libavdevice-dev libopencv-dev python-pip python-opencv
+sudo pip install scikit-learn
 cd dense_trajectory_release_v1.2/
 make clean
 make
