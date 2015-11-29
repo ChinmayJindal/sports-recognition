e68882d308f720545e00216c222de5051a4ab0ac
Chinmay <cs12b1012@iith.ac.in>
2015-11-29 19:45:05 +0530

    pipeline completed

--- collect.py
+++ collect.py
@@ -2,7 +2,7 @@ import os, time
 import pickle
 import Util
 import numpy as np
-from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 from sklearn.metrics.pairwise import chi2_kernel
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.neighbors import KDTree
@@ -47,9 +47,11 @@ class Video:
 class Action:
 	id = 0
 	def __init__(self, path, name):
+		print "Starting action : " + name + " ",
 		self.root = path
 		self.name = name
 		self.id = Action.id
+		print self.id
 		Action.id += 1
 		self.getVideoPaths()
 		self.traintestSplit()
@@ -59,6 +61,7 @@ class Action:
 		videoDirs = map(lambda p: self.root + os.path.sep + p, videoDirs)
 		self.videos = []
 		for v in videoDirs:
+			print v
 			video = filter(lambda p: p.endswith('.avi'), os.listdir(v))
 			if len(video) < 1:
 				continue
@@ -67,10 +70,9 @@ class Action:
 
 	def traintestSplit(self):
 		shuffle(self.videos)
-		numTrain = math.ceil(0.3*len(self.videos))
+		numTrain = int(math.ceil(0.3*len(self.videos)))
 		self.traindata = self.videos[:numTrain+1]
 		self.testdata = self.videos[numTrain+1:]
-
 
 #
 # Parameters:
@@ -84,11 +86,13 @@ def main(root):
 	actionDirs = map(lambda p: root + os.path.sep + p, actionDirs)
 	actions = map(lambda a, n: Action(a, n), actionDirs, actionNames)
 
+	print 'Collecting cluster features'
 	# collect data for generating clusters
 	clusterFeatures = np.empty([0,426], dtype=np.float32)
 	tags = []
 	for action in actions:
 		for video in action.traindata:
+			print video.src
 			start = clusterFeatures.shape[0]
 			clusterFeatures = np.vstack((clusterFeatures, video.features))
 			end = clusterFeatures.shape[1]
@@ -111,11 +115,8 @@ def main(root):
 		trainLabels.append(t[2])
 	trainLabels = np.array(trainLabels)
 
-
 	print 'Training SVM model with chi-squared kernel'
-	# apply kernel on all data
-	K = chi2_kernel(trainData, gamma=.5)
-	model = OneVsRestClassifier(LinearSVC(random_state=0)).fit(K, trainLabels)
+	model = OneVsRestClassifier(SVC(kernel=chi2_kernel, random_state=0, class_weight='auto')).fit(trainData, trainLabels)
 	pickle.dump(model, open('model.p', 'w'))
 	pickle.dump(centers, open('centers.p', 'w'))
 
@@ -130,8 +131,7 @@ def main(root):
 			testLabels.append(action.id)
 	testLabels = np.array(testLabels)
 
-	K = chi2_kernel(testData, gamma=.5)
-	predictedLabels = model.predict(K)	
+	predictedLabels = model.predict(testData)	
 
 	print "accuracy: " + str(float(np.sum(np.array(testLabels)==np.array(predictedLabels)))/predictedLabels.shape[0])
 
